31eb609578c7b29811fc22b42c7186c9693d5c6b
Vijay Prajapati <vijayprajapati71234@gmail.com>
2026-04-14 22:03:46 +0530

    Update yolo_train.py

--- yolo_train.py
+++ yolo_train.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8n.pt")
 
 # train model
 model.train(
-    data=r"C:\Users\praja\OneDrive\Desktop\python jupyter vs\ariel  project3\data.yaml",
+    data="data.yaml"
     epochs=20,
     imgsz=640
 )
